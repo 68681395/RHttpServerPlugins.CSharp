17485b972a51caadc12134a40b88f843efac5e66
rosenbjerg <malterandersen@live.dk>
2016-09-01 00:20:04 +0200

    migrated from other solution

---
+++ RHttpServerPlugins/MongoDB.RHttpServer/SimpleMongoDBConnection.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace RHttpServer.Plugins.External
+{
+    public class SimpleMongoDBConnection : RPlugin
+    {
+        private readonly MongoClient _client;
+
+        public SimpleMongoDBConnection(string url, int port = 80)
+        {
+            _client = new MongoClient($"{url}:{port}");
+        }
+
+        public IMongoDatabase GetDatabase(string db) => _client.GetDatabase(db);
+    }
+}
